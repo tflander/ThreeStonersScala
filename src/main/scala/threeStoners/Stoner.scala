5075ef00c867bb89cef7f4810571f6c116648ba1
Flanders, Todd <Todd.Flanders@cengage.com>
2015-09-04 12:39:01 -0400

    make little functions to handle each message type

--- src/main/scala/threeStoners/Stoner.scala
+++ src/main/scala/threeStoners/Stoner.scala
@@ -20,7 +20,7 @@ case class Stoner(supply: SmokingSupply) extends Actor {
   var supplyCount = 0
 
   var stoners: Seq[Stoner] = _
-  
+
   lazy val nextStoner = {
     val stonerCount = stoners.size
     val me = stoners.indexOf(this)
@@ -32,47 +32,57 @@ case class Stoner(supply: SmokingSupply) extends Actor {
 
   def processMessage(message: Message) = {
     require(message.to == stonerId, "invalid message:to.  Got " + message.to + ", expected " + stonerId)
+
+    def requestSupply {
+      println(message.from + " guy requested supply from " + message.to + " guy")
+      sendMessage(to = message.from, "takeSupply")
+    }
+
+    def takeSupply {
+      println(message.to + " guy takes supply from " + message.from + " guy")
+      supplyCount += 1
+      if (supplyCount == 2) {
+        sendMessage(to = stonerId, "roll")
+      }
+    }
+
+    def hitJoint(remainingTokes: Int) {
+      if (message.from == stonerId) {
+        println(message.to + " guy takes a toke")
+      } else {
+        println(message.to + " guy takes joint with " + remainingTokes + " hits left from " + message.from + " guy and tokes")
+      }
+      if (remainingTokes == 1) {
+        sendMessage(to = stonerId, "yourTurnToRoll")
+      } else {
+        sendMessage(to = nextStoner, "hitJoint" + (remainingTokes - 1))
+      }
+    }
+
+    def yourTurnToRoll {
+      println(message.to + " guy needs to roll a joint")
+      supplyCount = 0
+      stoners.foreach(stoner => {
+        if (stoner.stonerId != stonerId) {
+          sendMessage(stoner.stonerId, "requestSupply")
+        }
+      })
+    }
+    
+    def roll {
+        val numTokes = Random.nextInt(5) + 7
+        println(message.to + " guy rolls a " + numTokes + " hit joint and lights it")
+        sendMessage(to = stonerId, "hitJoint" + numTokes)      
+    }
+
     val hitJointPattern = "hitJoint(\\d+)".r
 
     message.message match {
-      case "requestSupply" => {
-        println(message.from + " guy requested supply from " + message.to + " guy")
-        sendMessage(to = message.from, "takeSupply")
-      }
-      case "takeSupply" => {
-        println(message.to + " guy takes supply from " + message.from + " guy")
-        supplyCount += 1
-        if (supplyCount == 2) {
-          sendMessage(to = stonerId, "roll")
-        }
-      }
-      case hitJointPattern(tokes) => {
-        if (message.from == stonerId) {
-          println(message.to + " guy takes a toke")
-        } else {
-          println(message.to + " guy takes joint with " + tokes + " hits left from " + message.from + " guy and tokes")
-        }
-        val remainingTokes = tokes.toInt
-        if (remainingTokes == 1) {
-          sendMessage(to = stonerId, "yourTurnToRoll")
-        } else {
-          sendMessage(to = nextStoner, "hitJoint" + (remainingTokes - 1))
-        }
-      }
-      case "yourTurnToRoll" => {
-        println(message.to + " guy needs to roll a joint")
-        supplyCount = 0
-        stoners.foreach(stoner => {
-          if (stoner.stonerId != stonerId) {
-            sendMessage(stoner.stonerId, "requestSupply")
-          }
-        })
-      }
-      case "roll" => {
-        val numTokes = Random.nextInt(5) + 7
-        println(message.to + " guy rolls a " + numTokes + " hit joint and lights it")
-        sendMessage(to = stonerId, "hitJoint" + numTokes)
-      }
+      case "requestSupply" => requestSupply
+      case "takeSupply" => takeSupply
+      case hitJointPattern(tokes) => hitJoint(tokes.toInt)
+      case "yourTurnToRoll" => yourTurnToRoll
+      case "roll" => roll
       case unknownMessage => {
         println("ignoring message " + unknownMessage + " from: " + message.from + " to: " + message.to)
       }
@@ -84,13 +94,13 @@ case class Stoner(supply: SmokingSupply) extends Actor {
   }
 
   override def act() {
-    
+
     react {
       case message: Message => {
         processMessage(message)
         act()
       }
       case "EXIT" => println(stonerId + " guy exiting")
-    }    
+    }
   }
 }
